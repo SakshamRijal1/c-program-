18e57a5326a737d1de64aa0554eca45e1949d93b
SakshamRijal1 <sakshamrijal07@gmail.com>
2024-12-22 22:46:56 +0545

    Add files via upload

---
+++ array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+
+int num[10],i; 
+printf("\n enter numbers");
+for(i=0;i<10;i++){
+
+
+	
+	scanf("%d",&num[i]);}
+	for(i=0;i<10;i++){
+	
+
+	printf("\n %d",num[i]);
+}
+
+	
+
+
+	return 0;
+}
