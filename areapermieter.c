d35d795c9668563c1ac9d9e434cbedaa2009319a
SakshamRijal1 <sakshamrijal07@gmail.com>
2025-02-17 02:50:21 +0545

    Create areapermieter.c

---
+++ areapermieter.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#define pie 3.1415
+main()
+{
+	float r,a,p;
+	printf("Enter the radius of circle");
+	scanf("%f",&r);
+	p=2*pie*r;
+a=pie*r*r;
+	printf("the area and perimeter of circle is %f %f",a,p);
+	
+}
