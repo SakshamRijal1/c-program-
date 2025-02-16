0b65a0fe2e9a8757ed7fa08b1d96dcc40a1e6342
SakshamRijal1 <sakshamrijal07@gmail.com>
2025-02-17 03:48:17 +0545

    Update matrixsum.c

--- matrixsum.c
+++ matrixsum.c
@@ -1,61 +1,61 @@
-#include<stdio.h>
+#include <stdio.h>
+
 #define m 3
 #define n 3
-int main()
-{
 
- int a[m][n],b[m][n],i,j,c[m][n];
- for(i=0;i<3;i++)
- {
- 	for(j=0;j<3;j++)
- 	{
-	 
- 
- printf("\n enter a[%d][%d]",i,j);
- scanf("%d",&a[i][j]);
-}}
- for(i=0;i<3;i++)
- {
- 	for(j=0;j<3;j++)
- 	{
-	 
- 
- printf("\n enter b[%d][%d]",i,j);
- scanf("%d",&b[i][j]);
-}}
-printf("Matrix a of m*n is:");
-for(i=0;i<3;i++)
- {
- 	for(j=0;j<3;j++)
- 	{
-	 
- 
- printf("%d",a[i][j]);}
- printf("\n") ;}
- printf("\t Matrix b of m*n is:");
-for(i=0;i<3;i++)
- {
- 	for(j=0;j<3;j++)
- 	{
-	 
- 
- printf("%d",b[i][j]);
+int main() {
+    int a[m][n], b[m][n], c[m][n];
+    int i, j;
+
+    // Input for matrix a
+    for(i = 0; i < m; i++) {
+        for(j = 0; j < n; j++) {
+            printf("\nEnter a[%d][%d]: ", i, j);
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Input for matrix b
+    for(i = 0; i < m; i++) {
+        for(j = 0; j < n; j++) {
+            printf("\nEnter b[%d][%d]: ", i, j);
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Display matrix a
+    printf("\nMatrix a (%d x %d) is:\n", m, n);
+    for(i = 0; i < m; i++) {
+        for(j = 0; j < n; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Display matrix b
+    printf("\nMatrix b (%d x %d) is:\n", m, n);
+    for(i = 0; i < m; i++) {
+        for(j = 0; j < n; j++) {
+            printf("%d ", b[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Matrix addition (sum of a and b)
+    for(i = 0; i < m; i++) {
+        for(j = 0; j < n; j++) {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    // Display the result of matrix addition
+    printf("\nThe sum of the matrices is:\n");
+    for(i = 0; i < m; i++) {
+        for(j = 0; j < n; j++) {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
-printf("\n");
- }
- for(i=0;i<3;i++)
- {
- 	for(j=0;j<3;j++)
- 	{
-	 
- c[i][j]=a[i][j]+b[i][j];
- 
-}}
-printf("\n the sum of matrix is:");
-for(i=0;i<3;i++)
- {
- 	for(j=0;j<3;j++)
- 	{
- 		printf("%d",c[i][j]);
-	 }
-	 printf("\n");}
